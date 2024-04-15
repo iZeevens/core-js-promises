3fe42e42b5ab4fc0fcfa810d24c4417299491f5c
iZeevens <129598659+iZeevens@users.noreply.github.com>
2024-04-15 14:31:13 +0500

    feat: add getPromise

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -17,8 +17,15 @@
  * 0    => promise that will be fulfilled
  * 1    => promise that will be fulfilled
  */
-function getPromise(/* number */) {
-  throw new Error('Not implemented');
+function getPromise(number) {
+  const promise = new Promise(function a(resolve, reject) {
+    if (number > 1) {
+      resolve(number);
+    } else {
+      reject(number);
+    }
+  });
+  return promise;
 }
 
 /**
